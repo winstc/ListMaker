513e275f64671485d11c5c541b3448ee6614300f
Winston Cadwell <wcadwell@gmail.com>
2016-12-14 11:39:50 -0500

    add file_handler.py

---
+++ file_handler.py
@@ -0,0 +1,29 @@
+import csv
+
+class CSV_File():
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def wirte(self):
+        pass
+
+
+class XLSX_File():
+    def __init__(self):
+        pass
+
+    def create(self):
+        pass
+
+    def read(self):
+        pass
+
+    def write(self):
+        pass
+
