26e4af3e70f71b507162bb7e78c4c1e7f9d230e0
Winston Cadwell <wcadwell@gmail.com>
2017-01-12 14:55:32 -0500

    add Comments

--- file_handler.py
+++ file_handler.py
@@ -1,36 +1,41 @@
 #!/usr/bin/python3.5
+# Written by Winston Cadwell
+# file_handler classes used by List Maker to manipulate files
+# these classes are used for all file actions in List Maker
+# The CSVFile class contains files for csv file manipulation
+# The XLSX class contains files for excel file manipulation
 
-import csv
-import xlsxwriter
+import csv  # import csv library for csv file manipulation
+import xlsxwriter  # import xlsx library for excel file manipulation
 
 
-class CSVFile:
+class CSVFile:  # class for read/writing of .csv files
     def __init__(self, file):
-        self.file = file
+        self.file = file  # store the requested file path
 
-    def read(self):
-        out = []
-        with open(self.file) as csvFile:
-            for row in csv.reader(csvFile):
-                out += [row]
+    def read(self):  # reads csv file into a list
+        out = []  # list to store read data
+        with open(self.file) as csvFile:  # open the file
+            for row in csv.reader(csvFile):  # for each row in the file
+                out += [row]  # append it to the list
 
-        return out
+        return out  # return the list
 
-    def write(self, data):
-        with open(self.file, 'w') as csvFile:
-            csv_write = csv.writer(csvFile)
-            for row in data:
-                csv_write.writerow(row)
+    def write(self, data):  # writes csv file
+        with open(self.file, 'w') as csvFile:  # open the file
+            csv_write = csv.writer(csvFile)  # create a new writer
+            for row in data:  # row each row
+                csv_write.writerow(row)  # write the row
 
 
-class XLSXFile:
+class XLSXFile:  # class for read/writing of .xlsx files
     def __init__(self, path, worksheet):
-        self.workbook = xlsxwriter.Workbook(path)
-        self.worksheet = self.workbook.add_worksheet(worksheet)
+        self.workbook = xlsxwriter.Workbook(path)  # set up the workbook
+        self.worksheet = self.workbook.add_worksheet(worksheet)  # add a worksheet
 
-    def write(self, data):
-        for index, row in enumerate(data):
-            self.worksheet.write_row('A' + str(index + 1), row)
+    def write(self, data):  # writes xlsx file
+        for index, row in enumerate(data):  # for each row in data
+            self.worksheet.write_row('A' + str(index + 1), row)  # write it the the spread sheet
 
-    def close(self):
-        self.workbook.close()
+    def close(self):  # closes the workbook
+        self.workbook.close()  # close the workbook
