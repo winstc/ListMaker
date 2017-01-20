02b35aec8e743430494ec89932cd8733d5a5d0b4
Winston Cadwell <wcadwell@gmail.com>
2017-01-20 10:19:37 -0500

    commit changes before moving repository to different account

--- insertDates.py
+++ insertDates.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout,QDialogButtonBox, QDateTimeEdit, QApplication, QPushButton
+from PyQt5.QtWidgets import QDialog, QVBoxLayout, QHBoxLayout, QCalendarWidget, QPushButton, QRadioButton, QLabel, QSpinBox
 from PyQt5.QtCore import Qt, QDateTime
 import sys
 
@@ -13,6 +13,45 @@ class insertDates(QDialog):
         self.setWindowModality(Qt.ApplicationModal)
         self.resize(400,400)
 
+        intervalLabel = QLabel("Rotation Interval:")
+        mRotationsRBtn = QRadioButton("Monthly")
+        wRotationsRBtn = QRadioButton("Weekly")
+        dRotationsRBtn = QRadioButton("Daily")
+        cRotationsRBtn = QRadioButton("Custom:")
+        customEntry = QSpinBox()
+        customEntry.setRange(0, 365)
+        customEntry.setFixedWidth(80)
+
+        intervalBox = QVBoxLayout()
+        intervalBox.addWidget(intervalLabel)
+        intervalBox.addWidget(mRotationsRBtn)
+        intervalBox.addWidget(wRotationsRBtn)
+        intervalBox.addWidget(dRotationsRBtn)
+        intervalBox.addWidget(cRotationsRBtn)
+        intervalBox.addWidget(customEntry)
+
+        startCalLabel = QLabel("Start Date:")
+
+        startCal = QCalendarWidget()
+        startCal.setGridVisible(True)
+
+        startCalBox = QVBoxLayout()
+        startCalBox.addWidget(startCalLabel)
+        startCalBox.addWidget(startCal)
+
+        endCalLabel = QLabel("End Date:")
+
+        endCal = QCalendarWidget()
+        endCal.setGridVisible(True)
+
+        endCalBox = QVBoxLayout()
+        endCalBox.addWidget(endCalLabel)
+        endCalBox.addWidget(endCal)
+
+        calBox = QHBoxLayout()
+        calBox.addLayout(startCalBox)
+        calBox.addLayout(endCalBox)
+
         okButton = QPushButton("OK", self)
 
         closeButton = QPushButton("Close", self)
@@ -23,7 +62,9 @@ class insertDates(QDialog):
         btnBox.addWidget(closeButton)
 
         vertBox = QVBoxLayout()
-        vertBox.addStretch(1)
+        vertBox.addLayout(intervalBox)
+        vertBox.insertSpacing(20, 20)
+        vertBox.addLayout(calBox)
         vertBox.addLayout(btnBox)
 
         self.setLayout(vertBox)
